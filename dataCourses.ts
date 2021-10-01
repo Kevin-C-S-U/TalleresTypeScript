687de0d9fff58e968a510a1829bc87d5e963470d
Kevin-C-S-U <88560219+Kevin-C-S-U@users.noreply.github.com>
2021-09-30 23:11:44 -0500

    Add files via upload

---
+++ dataCourses.ts
@@ -0,0 +1,10 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Desarrollo de Software en Equipo", "Cesar Forero", 3),
+  new Course("Cálculo Vectorial", "Sergio Adarve", 3),
+  new Course("Biología Molécular", "María del pilar", 3),
+  new Course("Diseño y Análisis de Algoritmos", "Jorge Duitama", 3),
+  new Course("T.I. En las Organizaciones", "Julían Moreno", 3),
+  new Course("Sistemas Transaccionales", "Claudia Jimenez", 3)
+] 
